aa020566cca879dece2194bcddd4c4a4af36bbf6
Devashree95 <122653285+Devashree95@users.noreply.github.com>
2023-11-19 22:37:53 -0500

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,14 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+class preprocess:
+
+    def chunk_section(section, chunk_size, chunk_overlap):
+        text_splitter = RecursiveCharacterTextSplitter(
+            separators=["\n\n", "\n", " ", ""],
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            length_function=len)
+        chunks = text_splitter.create_documents(
+            texts=[section["text"]],
+            metadatas=[{"source": section["source"]}])
+        return [{"text": chunk.page_content, "source": chunk.metadata["source"]} for chunk in chunks]
