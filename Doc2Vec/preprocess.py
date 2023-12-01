87f5d67faa8d9d8e85401faa29db31daf047e7af
Devashree95 <122653285+Devashree95@users.noreply.github.com>
2023-12-01 13:16:20 -0500

    Update preprocess.py

--- Doc2Vec/preprocess.py
+++ Doc2Vec/preprocess.py
@@ -2,13 +2,16 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 class preprocess:
 
+    # This function divides a section of text into smaller chunks.
     def chunk_section(section, chunk_size, chunk_overlap):
         text_splitter = RecursiveCharacterTextSplitter(
-            separators=["\n\n", "\n", " ", ""],
-            chunk_size=chunk_size,
-            chunk_overlap=chunk_overlap,
+            separators=["\n\n", "\n", " ", ""], # Define separators for splitting text.
+            chunk_size=chunk_size,              # Maximum size of each chunk.
+            chunk_overlap=chunk_overlap,        # Number of characters to overlap between chunks.
             length_function=len)
+        # Use the text splitter to divide the input text into chunks.
         chunks = text_splitter.create_documents(
             texts=[section["text"]],
             metadatas=[{"source": section["source"]}])
+        # Return a list of dictionaries, each containing a chunk of text and its associated metadata.
         return [{"text": chunk.page_content, "source": chunk.metadata["source"]} for chunk in chunks]
