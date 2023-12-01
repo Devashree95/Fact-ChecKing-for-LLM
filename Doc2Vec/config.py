66f9e0d6d4285b5f49de024bc5fc77e7ba343c23
Devashree95 <122653285+Devashree95@users.noreply.github.com>
2023-12-01 12:51:48 -0500

    Update config.py

--- Doc2Vec/config.py
+++ Doc2Vec/config.py
@@ -1,14 +1,17 @@
 from supabase import create_client
 
 ### Input sources
-ai_book_path = 'D:\ML2\RAG\data\AI-book.pdf'
+ai_book_path = '/data'
 base_url= "https://www.geeksforgeeks.org"
 web_url = 'https://www.geeksforgeeks.org/machine-learning/'
 
 ### SUPABASE DB
+
+#Please provide you own URL and key here while running
 supabase_url = "https://mufqacshyjgmzivznuwo.supabase.co"
-supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11ZnFhY3NoeWpnbXppdnpudXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg0MzM2MTQsImV4cCI6MjAxNDAwOTYxNH0.iYqEsEQJWcQUu7KjYWBzrRKdZn23Vp_xlhkHomc85Tc"
+supabase_key = "Please provide your key here"
 client = create_client(supabase_url, supabase_key)
+#Please change the table name to your table name
 table_name = "combined_test"
 
 headers = {
@@ -16,8 +19,13 @@ headers = {
     "Content-Type": "application/json"
 }
 
+# Please change the table name to your table name
 endpoint = f"{supabase_url}/rest/v1/combined_test"
 
+### Config for text splitter
 # Text splitter
 chunk_size = 1000
 chunk_overlap = 50
+
+### Open AI
+openai_key = "Please provide open AI key here"
