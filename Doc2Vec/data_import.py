21ffc74c8a7c188c9f2564ae877236ca82ff5f70
Devashree95 <122653285+Devashree95@users.noreply.github.com>
2023-12-01 13:05:40 -0500

    Update data_import.py

--- Doc2Vec/data_import.py
+++ Doc2Vec/data_import.py
@@ -1,6 +1,6 @@
+# Import necessary libraries and modules.
 import config
 import create_embeddings
-#import storeToDB
 import requests
 from bs4 import BeautifulSoup
 import httpx
@@ -8,34 +8,37 @@ import json
 import preprocess
 from gensim.models.doc2vec import Doc2Vec
 
+# Load configurations and initialize variables.
 pdf_path = config.ai_book_path
 url = config.web_url
 embedding = create_embeddings.embedding
-# store= storeToDB.saveToDb
 chunking = preprocess.preprocess
 model = Doc2Vec.load("jesc102_model.d2v")
 
+# Class for importing and processing data from PDF and web sources.
 class dataImport:
-
+    # Method for importing and processing data from a PDF file.
     def importPdf(pdf):
         tagged_data = list(create_embeddings.embedding.extract_text_by_page(pdf))
-        model = Doc2Vec.load("jesc102_model.d2v")
-        pages_text = list(tagged_data)
+        model = Doc2Vec.load("jesc102_model.d2v") # Load the saved model
+        pages_text = list(tagged_data) 
         for i in range(len(tagged_data)):
-            vector = model.dv[i].tolist()  # Convert to list assuming it's a numpy array
-            tag = i
+            vector = model.dv[i].tolist()  # Convert document vector to list.
+            tag = i  # Page number as the tag.
 
-            # Convert your vector to a JSON string.
+            # # Prepare data for insertion into the database
             data_to_insert = {
                 "source": tag,
                 "text": pages_text[i],
                 "vector": json.dumps(vector)
             }
 
+            # Send a POST request to store the vector data
             response = httpx.post(config.endpoint, headers=config.headers, json=data_to_insert)
             print(f"Stored vector for '{tag}': {response}")
         return tagged_data
 
+    # Method for importing and processing data from a URL.
     def importUrl():
         response = requests.get(url)
         sections = []
@@ -65,32 +68,29 @@ class dataImport:
 
                     # Combine all paragraph texts into a single string
                     full_text = '\n\n'.join(paragraph_texts)
+
+                    # create a section with text and source field
                     section = {"text": full_text, "source": full_url}
+                    # combine all sections with their respective sources
                     sections.append({"text": full_text, "source": full_url})
                     chunks_ds = chunking.chunk_section(section, chunk_size=config.chunk_size,
                                                        chunk_overlap=config.chunk_overlap)
+                    # Load data chunks for each section to Supabase DB
                     for chunk in chunks_ds:
                         vector = model.infer_vector(embedding.preprocess_sentence(chunk["text"])).tolist()
                         vector_data = json.dumps(vector)
-                        # print(vector_data)
 
+                        Prepare the data to be inserted 
                         data_to_insert = {
                             "source": chunk["source"],
                             "text": chunk["text"],
                             "vector": vector_data
                         }
 
-                        # response = client.from_(table_name).upsert([vector_data]).execute()
-
+                        # Send a POST request to store the vector data
                         response = httpx.post(config.endpoint, headers=config.headers, json=data_to_insert)
                 else:
                     print(f"Failed to retrieve the page from {full_url}. Status code: {page_response.status_code}")
 
         else:
             print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
-
-
-
-
-
-
