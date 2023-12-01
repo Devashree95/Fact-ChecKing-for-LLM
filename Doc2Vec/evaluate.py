f9fa2f71fe1e24b14aa2be3ebfddd2f347b61e99
Devashree95 <devashree.bhagwat95@gmail.com>
2023-12-01 10:03:59 -0500

    dd

---
+++ Doc2Vec/evaluate.py
@@ -0,0 +1,18 @@
+import config
+import create_embeddings
+from gensim.models.doc2vec import Doc2Vec
+import data_import
+
+pdf_path = config.ai_book_path
+embedding = create_embeddings.embedding
+# Load the model
+model = Doc2Vec.load("jesc102_model.d2v")
+
+new_sentence = "Hello."
+vector = model.infer_vector(embedding.preprocess_sentence(new_sentence))
+print(vector)
+
+# data_import.dataImport.importUrl()
+
+data_import.dataImport.importPdf(pdf_path)
+
---
+++ Doc2Vec/train.py
@@ -0,0 +1,15 @@
+from data_import import dataImport
+import config
+from gensim.models.doc2vec import Doc2Vec
+
+pdf_path = config.ai_book_path
+
+tagged_data = dataImport.importPdf(pdf_path)
+
+model = Doc2Vec(vector_size=50, window=5, min_count=5, workers=4, epochs=100)
+model.build_vocab(tagged_data)
+model.train(tagged_data, total_examples=model.corpus_count, epochs=model.epochs)
+
+# Save the model
+model.save("jesc102_model.d2v")
+
