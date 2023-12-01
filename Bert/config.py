9532880a53c894580953abe6518aae60600651b2
Devashree95 <122653285+Devashree95@users.noreply.github.com>
2023-12-01 12:35:17 -0500

    Create config.py

---
+++ Bert/config.py
@@ -0,0 +1,13 @@
+### Supabase
+SUPABASE_URL = "https://tmwcifbhcnkaiqsldmlz.supabase.co"
+SUPABASE_SERVICE_API_KEY = "Please provide your key here"
+
+### Open AI
+openai_key = 'Please provide your key here'
+
+dir_path = '/data'
+supabase_table_name = "books_pretrained_embeddings"
+## This is cosine similarity match function in supabase
+match_function_name = "match_books_pretrained_embeddings"
+## Pretrained model name from Hugging face
+model_name = 'sentence-transformers/all-mpnet-base-v2'
